f50262f2e97d6a8bba7b0a35eeaf540cf9fd9269
Dawson Hensel <dawsonhensel@gmail.com>
2024-02-24 14:39:59 -0800

    Improve onboarding process and user console messages

--- cmd/utils/utils.go
+++ cmd/utils/utils.go
@@ -2,10 +2,27 @@ package utils
 
 import (
 	"errors"
+	"fmt"
+	"log"
 	"os"
 )
 
 func FileExists(pathWithFilename string) bool {
 	_, err := os.Open(pathWithFilename)
 	return !errors.Is(err, os.ErrNotExist)
+}
+
+func PrintFinalError(message string, err error) {
+	if message == "" {
+		message = "A fatal error occurred"
+	}
+	if err == nil {
+		log.Printf(message)
+	} else {
+		log.Printf("%s: %s", message, err)
+	}
+	fmt.Println("Press Enter key to exit...")
+	var noop string
+	fmt.Scanln(&noop)
+	os.Exit(1)
 }
--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"dhes/emissary/daemon/cmd/utils"
 	"fmt"
 	"io"
-	"log"
 	"log/slog"
 	"net"
 	"os"
@@ -19,50 +18,48 @@ func main() {
 	fmt.Println("Emissary is starting...")
 	_, err := os.Stat(certificatesAndKeysFolderName)
 	if os.IsNotExist(err) {
-		if err := os.Mkdir(certificatesAndKeysFolderName, os.ModePerm); err != nil {
-			log.Fatal("Unable to create put_certificates_and_key_from_drawbridge_here folder! This folder is required to exist, so please create it yourself or allow Emissary the permissions required to create it.")
-		}
+		runOnboarding()
 	}
 
 	fmt.Println("Emissary is trying to read the Certificate file...")
 	if !utils.FileExists("./put_certificates_and_key_from_drawbridge_here/emissary-mtls-tcp.crt") {
-		message := fmt.Sprintf(`The "emissary-mtls-tcp.crt" file is missing from the "%s" folder, which should be next to this program.
-		To generate this file, please log into the Drawbridge Dashboard and click the "Generate" button.
-		Once that is done, please place those files into the "put_certificates_and_key_from_drawbridge_here" folder and run Emissary again.`, certificatesAndKeysFolderName)
-		log.Fatal(message)
+		message := fmt.Sprintf("The \"emissary-mtls-tcp.crt\" file is missing from the \"%s\" folder, which should be next to this program.\n", certificatesAndKeysFolderName)
+		message += "To generate this file, please log into the Drawbridge Dashboard and click the \"Generate\" button.\n"
+		message += "Once that is done, please place those files into the \"put_certificates_and_key_from_drawbridge_here\" folder and run Emissary again.\n\n"
+		utils.PrintFinalError(message, nil)
 	}
 
 	fmt.Println("Emissary is trying to read the Key file...")
 	if !utils.FileExists("./put_certificates_and_key_from_drawbridge_here/emissary-mtls-tcp.key") {
-		message := fmt.Sprintf(`The "emissary-mtls-tcp.key" file is missing from the "%s" folder, which should be next to this program.
-		To generate this file, please log into the Drawbridge Dashboard and click the "Generate" button.
-		Once that is done, please place those files into the "put_certificates_and_key_from_drawbridge_here" folder and run Emissary again.`, certificatesAndKeysFolderName)
-		log.Fatal(message)
+		message := fmt.Sprintf("The \"emissary-mtls-tcp.key\" file is missing from the \"%s\" folder, which should be next to this program.\n", certificatesAndKeysFolderName)
+		message += "To generate this file, please log into the Drawbridge Dashboard and click the \"Generate\" button.\n"
+		message += "Once that is done, please place those files into the \"put_certificates_and_key_from_drawbridge_here\" folder and run Emissary again.\n\n"
+		utils.PrintFinalError(message, nil)
 	}
 
 	fmt.Println("Emissary is trying to read the CA Certificate file...")
 	if !utils.FileExists("./put_certificates_and_key_from_drawbridge_here/ca.crt") {
-		message := fmt.Sprintf(`The "ca.crt" file is missing from the "%s" folder, which should be next to this program.
-		To generate this file, please log into the Drawbridge Dashboard and click the "Generate" button.
-		Once that is done, please place those files into the "put_certificates_and_key_from_drawbridge_here" folder and run Emissary again.`, certificatesAndKeysFolderName)
-		log.Fatal(message)
+		message := fmt.Sprintf("The \"ca.crt\" file is missing from the \"%s\" folder, which should be next to this program.\n", certificatesAndKeysFolderName)
+		message += "To generate this file, please log into the Drawbridge Dashboard and click the \"Generate\" button.\n"
+		message += "Once that is done, please place those files into the \"put_certificates_and_key_from_drawbridge_here\" folder and run Emissary again.\n\n"
+		utils.PrintFinalError(message, nil)
 	}
 
 	fmt.Println("Emissary is trying to load Certificate and Key file for connecting to Drawbridge...")
 	// load tls configuration
 	cert, err := tls.LoadX509KeyPair("./put_certificates_and_key_from_drawbridge_here/emissary-mtls-tcp.crt", "./put_certificates_and_key_from_drawbridge_here/emissary-mtls-tcp.key")
 	if err != nil {
-		log.Fatal(err)
+		utils.PrintFinalError("", err)
 	}
 	// Configure the client to trust TLS server certs issued by a CA.
 	certPool, err := x509.SystemCertPool()
 	if err != nil {
-		log.Fatal(err)
+		utils.PrintFinalError("", err)
 	}
 	if caCertPEM, err := os.ReadFile("./put_certificates_and_key_from_drawbridge_here/ca.crt"); err != nil {
-		log.Fatal(err)
+		utils.PrintFinalError("", err)
 	} else if ok := certPool.AppendCertsFromPEM(caCertPEM); !ok {
-		log.Fatal("invalid cert in CA PEM")
+		utils.PrintFinalError("invalid cert in CA PEM", nil)
 	}
 
 	tlsConfig := &tls.Config{
@@ -71,15 +68,16 @@ func main() {
 	}
 
 	fmt.Println()
-	fmt.Println("Please enter your Drawbridge server URL or IP (e.g drawbridge.mysite.com:8000 or 50.162.50.224:8000):")
+	fmt.Println("Please enter your Drawbridge server URL or IP (e.g drawbridge.mysite.com:3100 or 50.162.50.224:3100):")
+	fmt.Println("Please note the default Drawbridge reverse proxy port is 3100.")
 	var drawbridgeLocationResponse string
 	fmt.Scan(&drawbridgeLocationResponse)
 	fmt.Println()
 
-	fmt.Println("The remote protected service is now available at 127.0.0.1:8008 (If everything works).")
-	l, err := net.Listen("tcp", "127.0.0.1:8008")
+	fmt.Println("The remote protected service is now available at 127.0.0.1:4000 (If everything works).")
+	l, err := net.Listen("tcp", "127.0.0.1:4000")
 	if err != nil {
-		log.Fatalf("Emissary was unable to start the local proxy server: %s", err)
+		utils.PrintFinalError("Emissary was unable to start the local proxy server", err)
 	}
 
 	defer l.Close()
@@ -87,7 +85,7 @@ func main() {
 		// wait for connection
 		conn, err := l.Accept()
 		if err != nil {
-			log.Fatalf("Reverse proxy TCP Accept failed: %s", err)
+			utils.PrintFinalError("Reverse proxy TCP Accept failed", err)
 		}
 		// Handle new connection in a new go routine.
 		// The loop then returns to accepting, so that
@@ -96,7 +94,7 @@ func main() {
 			// connect to drawbridge on the port lsitening for the actual service
 			conn, err := tls.DialWithDialer(&net.Dialer{Timeout: 15 * time.Second}, "tcp", drawbridgeLocationResponse, tlsConfig)
 			if err != nil {
-				fmt.Errorf("Failed connecting to Drawbridge mTLS TCP server: %s", err)
+				utils.PrintFinalError("Failed connecting to Drawbridge mTLS TCP server", err)
 				return
 			}
 			defer conn.Close()
@@ -110,4 +108,22 @@ func main() {
 		}(conn)
 	}
 
+}
+
+func runOnboarding() {
+	fmt.Println("\n* * * * * * * * * * * *")
+	fmt.Println("  Welcome to Emissary!")
+	fmt.Println("* * * * * * * * * * * *")
+	fmt.Println("\nFIRST TIME SETUP INSTRUCTIONS:")
+	fmt.Println("Please have your Drawbridge admin give you the required key and certificate files; then")
+	fmt.Println("place them in the \"put_certificates_and_key_from_drawbridge_here\" folder we just created.")
+	fmt.Println("Once completed, run Emissary again to connect to your Protected Service!")
+	fmt.Println("\nPress Enter key to exit...")
+	if err := os.Mkdir(certificatesAndKeysFolderName, os.ModePerm); err != nil {
+		utils.PrintFinalError("Unable to create put_certificates_and_key_from_drawbridge_here folder! This folder is required to exist, so please create it yourself or allow Emissary the permissions required to create it.", nil)
+	} else {
+		var noop string
+		fmt.Scanln(&noop)
+		os.Exit(0)
+	}
 }
