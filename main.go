8819385f30abda098fb293d7845bd3421b033566
Dawson Hensel <dawsonhensel@gmail.com>
2024-02-23 18:43:40 -0800

    Have help output match the drawbridge ui element

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 	fmt.Println("Emissary is trying to read the Certificate file...")
 	if !utils.FileExists("./put_certificates_and_key_from_drawbridge_here/emissary-mtls-tcp.crt") {
 		message := fmt.Sprintf(`The "emissary-mtls-tcp.crt" file is missing from the "%s" folder, which should be next to this program.
-		To generate this file, please log into the Drawbridge Dashboard and click the "Generate Emissary Client and Certificate Files" button.
+		To generate this file, please log into the Drawbridge Dashboard and click the "Generate Emissary Client Certificate and Key Files" button.
 		Once that is done, please place those files into the "put_certificates_and_key_from_drawbridge_here" folder and run Emissary again.`, certificatesAndKeysFolderName)
 		log.Fatal(message)
 	}
@@ -63,7 +63,7 @@ func main() {
 	}
 
 	fmt.Println()
-	fmt.Println("Please enter your Drawbridge server IP or URL (http://drawbridge.mysite.com:8000 or 50.162.50.224:8000):")
+	fmt.Println("Please enter your Drawbridge server URL or IP (e.g drawbridge.mysite.com:8000 or 50.162.50.224:8000):")
 	var drawbridgeLocationResponse string
 	fmt.Scan(&drawbridgeLocationResponse)
 	fmt.Println()
