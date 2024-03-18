5de749d8f44a5cc7d2f062bba53cca9685744eee
Dawson Hensel <dawsonhensel@gmail.com>
2024-03-18 11:17:19 -0700

    Update build scripts

--- build/linux_amd64.sh
+++ build/linux_amd64.sh
@@ -1,1 +1,3 @@
-GOARCH=amd64 GOOS=linux go build -o release/Emissary_linux_amd64 -ldflags="-s -w" .
+GOARCH=amd64 GOOS=linux go build -o release/Emissary_linux_amd64_$1 -ldflags="-s -w" .
+zip -r ./release/Emissary_linux_amd64_$1.zip release/Emissary_linux_amd64_$1
+gpg --output release/Emissary_linux_amd64_$1.zip.sig --detach-sig release/Emissary_linux_amd64_$1.zip
--- build/macos_arm64.sh
+++ build/macos_arm64.sh
@@ -1,1 +1,3 @@
-GOARCH=arm64 GOOS=darwin go build -o ./release/Emissary_macos_arm64 -ldflags="-s -w" .
+GOARCH=arm64 GOOS=darwin go build -o release/Emissary_macos_arm64_$1 -ldflags="-s -w" .
+zip -r ./release/Emissary_macos_arm64_$1.zip release/Emissary_macos_arm64_$1
+gpg --output release/Emissary_macos_arm64_$1.zip.sig --detach-sig release/Emissary_macos_arm64_$1.zip
