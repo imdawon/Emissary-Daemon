db949685918b1ce7a5b665fcb98469fb0d377215
Dawson Hensel <dawsonhensel@gmail.com>
2024-03-24 15:13:31 -0700

    Use .asc instead of .sig for signing files

--- build/linux_amd64.sh
+++ build/linux_amd64.sh
@@ -1,3 +1,3 @@
 GOARCH=amd64 GOOS=linux go build -o release/Emissary_linux_amd64_$1 -ldflags="-s -w" .
 zip -r ./release/Emissary_linux_amd64_$1.zip release/Emissary_linux_amd64_$1
-gpg --output release/Emissary_linux_amd64_$1.zip.sig --detach-sig release/Emissary_linux_amd64_$1.zip
+gpg --armor --output release/Emissary_linux_amd64_$1.zip.asc --detach-sig release/Emissary_linux_amd64_$1.zip
--- build/windows_amd64.sh
+++ build/windows_amd64.sh
@@ -1,3 +1,3 @@
 CGO_ENABLED=1 CC="x86_64-w64-mingw32-gcc" GOARCH=amd64 GOOS=windows go build -o release/Emissary_windows_amd64_$1.exe -ldflags="-s -w" .
 zip -r ./release/Emissary_windows_amd64_$1.zip release/Emissary_windows_amd64_$1.exe
-gpg --output release/Emissary_windows_amd64_$1.zip.sig --detach-sig release/Emissary_windows_amd64_$1.zip
+gpg --armor --output release/Emissary_windows_amd64_$1.zip.asc --detach-sig release/Emissary_windows_amd64_$1.zip
